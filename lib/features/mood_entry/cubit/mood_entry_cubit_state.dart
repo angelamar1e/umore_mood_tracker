4246bd385497a7c2d647e50bfd8f4a12bcc1eb9f
PLUS\Angela.Delapena <angela.delapena@tbu.net>
2025-04-23 16:23:37 +0800

    created mood_entry state

--- lib/features/mood_entry/cubit/mood_entry_cubit_state.dart
+++ lib/features/mood_entry/cubit/mood_entry_cubit_state.dart
@@ -1,6 +1,12 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 part of 'mood_entry_cubit_cubit.dart';
 
-@immutable
-sealed class MoodEntryCubitState {}
+class MoodEntryCubitState {
+  final MoodEntry entry;
 
-final class MoodEntryCubitInitial extends MoodEntryCubitState {}
+  MoodEntryCubitState({required this.entry});
+
+  MoodEntryCubitState copyWith({MoodEntry? entry}) {
+    return MoodEntryCubitState(entry: entry ?? this.entry);
+  }
+}
