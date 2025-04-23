e113bb2154cc380100f8e0fe1b8603fccedb286e
PLUS\Angela.Delapena <angela.delapena@tbu.net>
2025-04-23 16:50:11 +0800

    setup history state

--- lib/features/mood_history/cubit/mood_history_cubit.dart
+++ lib/features/mood_history/cubit/mood_history_cubit.dart
@@ -1,8 +1,8 @@
-import 'package:bloc/bloc.dart';
-import 'package:meta/meta.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:umore_mood_tracker/features/mood_entry/models/mood_entry.dart';
 
 part 'mood_history_state.dart';
 
 class MoodHistoryCubit extends Cubit<MoodHistoryState> {
-  MoodHistoryCubit() : super(MoodHistoryInitial());
+  MoodHistoryCubit() : super(MoodHistoryInitial(historyList: List.empty()));
 }
--- lib/features/mood_history/cubit/mood_history_state.dart
+++ lib/features/mood_history/cubit/mood_history_state.dart
@@ -1,6 +1,16 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 part of 'mood_history_cubit.dart';
 
-@immutable
-sealed class MoodHistoryState {}
+class MoodHistoryState {
+  final List<MoodEntry> historyList;
 
-final class MoodHistoryInitial extends MoodHistoryState {}
+  MoodHistoryState({required this.historyList});
+
+  MoodHistoryState copyWith({List<MoodEntry>? historyList}) {
+    return MoodHistoryState(historyList: historyList ?? this.historyList);
+  }
+}
+
+final class MoodHistoryInitial extends MoodHistoryState {
+  MoodHistoryInitial({required super.historyList});
+}
