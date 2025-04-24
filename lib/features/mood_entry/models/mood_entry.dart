6d1bc28c603494af88ce810b513a964f1edb98b0
PLUS\Angela.Delapena <angela.delapena@tbu.net>
2025-04-24 09:40:01 +0800

    entry model changed to have moodkey

--- lib/features/mood_entry/models/mood_entry.dart
+++ lib/features/mood_entry/models/mood_entry.dart
@@ -1,10 +1,11 @@
-// ignore_for_file: public_member_api_docs, sort_constructors_first
-import 'package:umore_mood_tracker/features/mood_entry/models/mood.dart';
-
 class MoodEntry {
-  Mood mood;
+  String moodKey;
   String notes;
   DateTime timestamp;
 
-  MoodEntry({required this.mood, required this.notes, required this.timestamp});
+  MoodEntry({
+    required this.moodKey,
+    required this.notes,
+    required this.timestamp,
+  });
 }
