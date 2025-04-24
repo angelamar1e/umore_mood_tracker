839711918a2ecb25ed9521dbab4e7852724ec3aa
PLUS\Angela.Delapena <angela.delapena@tbu.net>
2025-04-24 14:15:41 +0800

    create list of pie section data, for chart data

--- lib/features/mood_stats/cubit/mood_stats_cubit.dart
+++ lib/features/mood_stats/cubit/mood_stats_cubit.dart
@@ -4,6 +4,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:umore_mood_tracker/features/mood_entry/models/mood.dart';
 import 'package:umore_mood_tracker/features/mood_entry/models/mood_entry.dart';
+import 'package:umore_mood_tracker/features/mood_stats/models/pie_scction_data.dart';
+import 'package:umore_mood_tracker/shared/constants/mood_types.dart';
+import 'package:umore_mood_tracker/shared/database/database_helper.dart';
 
 part 'mood_stats_state.dart';
 
@@ -20,4 +23,20 @@ class MoodStatsCubit extends Cubit<MoodStatsState> {
   /*TODO: Methods - get history list from db, convert to display as trends, get average, get most frequent 
 
   */
+
+  void getChartData() async {
+    final moodTypeCount = await countByMood();
+    late List<PieSectionData> pieChartData = [];
+
+    for (MapEntry<String, int> entry in moodTypeCount.entries) {
+      final percentage = entry.value * 100 / moodTypeCount.length;
+      pieChartData.add(
+        PieSectionData(
+          image: moodTypes[entry.key]!.image,
+          value: percentage,
+          title: '$percentage%',
+        ),
+      );
+    }
+  }
 }
