efdd6c1938edacb64371d18139d9f9d10bf380ce
PLUS\Angela.Delapena <angela.delapena@tbu.net>
2025-04-25 16:34:11 +0800

    added color property

--- lib/features/mood_stats/models/pie_section_data.dart
+++ lib/features/mood_stats/models/pie_section_data.dart
@@ -1,9 +1,14 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:flutter/material.dart';
+
 class PieSectionData {
+  Color color;
   String image;
   double value;
   String title;
 
   PieSectionData({
+    required this.color,
     required this.image,
     required this.value,
     required this.title,
